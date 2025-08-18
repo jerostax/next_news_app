4659e117a7ba443f5d3adfa4d70576c124b71efa
Jeremy <jeremy.geneste@gmail.com>
2025-08-18 11:41:47 +0200

    feature: added news & news details pages

---
+++ app/news/[id]/page.js
@@ -0,0 +1,8 @@
+export default function NewsDetailPage() {
+  return (
+    <div>
+      <h1>News Detail Page</h1>
+      <p>Welcome to the news detail section of our application!</p>
+    </div>
+  );
+}
---
+++ app/news/page.js
@@ -0,0 +1,8 @@
+export default function NewsPage() {
+  return (
+    <div>
+      <h1>News Page</h1>
+      <p>Welcome to the news section of our application!</p>
+    </div>
+  );
+}
