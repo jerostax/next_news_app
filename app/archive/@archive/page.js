63ed6bef6bdca4e35c056a4afd4ea87964909be9
Jeremy <jeremy.geneste@gmail.com>
2025-08-19 12:54:58 +0200

    feature: added parallel routes for archives

---
+++ app/archive/@archive/page.js
@@ -0,0 +1,8 @@
+export default function ArchivePage() {
+  return (
+    <div className="archive-page">
+      <h1>Archive</h1>
+      <p>This is the archive page. Content will be added soon.</p>
+    </div>
+  );
+}
