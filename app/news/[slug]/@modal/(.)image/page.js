9d3f50624d91720ca9684309c7a951f8147a8137
Jeremy <jeremy.geneste@gmail.com>
2025-08-19 14:56:33 +0200

    feature: navigate back via useRouter when clicking on modal backdrop

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,16 +1,21 @@
-import { notFound } from "next/navigation";
+"use client";
+
+import { notFound, useRouter } from "next/navigation";
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
+
   const { slug } = params;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
 
   if (!newsItem) {
     notFound(); // This will trigger the not-found page
   }
+
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
