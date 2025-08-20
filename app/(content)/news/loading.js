8efaca58e1a160d0ceedcde8d50cdc4c02689f81
Jeremy <jeremy.geneste@gmail.com>
2025-08-20 15:50:47 +0200

    feature: added a loading fallback

---
+++ app/(content)/news/loading.js
@@ -0,0 +1,8 @@
+export default function NewsLoading() {
+  return (
+    <div className="loading">
+      <h1>Loading News...</h1>
+      <p>Please wait while we fetch the latest news updates.</p>
+    </div>
+  );
+}
